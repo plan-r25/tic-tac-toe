9e9aeef986d615828cc946a43485c94942fe5103
planR <rplann24@gmail.com>
2026-06-08 16:43:09 +0000

    playing around with js

--- script.js
+++ script.js
@@ -3,13 +3,18 @@ const parent = document.querySelector(".parent");
 for (let i = 0; i < 9; i++) {
 
   const div = document.createElement("button");
-  div.classList.add("item");
+    div.classList.add("item", `box${i}`);
   let p = document.createElement("p");
-  p.textContent = i;
+  // p.textContent = i;
   div.appendChild(p);
   parent.appendChild(div);
   
   div.addEventListener("click", (e) => {
-    console.log("you cliked:", e.target.textContent);
+    if(e.target.className) {
+      p.style.background = "blue";
+      div.disabled = true;
+      
+    }
+    console.log("you cliked:", e.target.className);
   })
 }
