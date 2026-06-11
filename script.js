a951092405e1138f05ff4570e07c70b2aea841f7
planR <rplann24@gmail.com>
2026-06-11 16:41:53 +0000

    still haven't figure how to connect the two arrays pattern

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const msg = document.querySelector("#msg");
 const board = document.querySelector("#board");
 const restartBtn = document.querySelector(".restart");
 const newBtn = document.querySelector(".new-game");
+const choiceBtn = document.querySelectorAll(".btn");
+
 let xTurn = true;
 
 const winPatterns = [
@@ -17,10 +19,38 @@ const winPatterns = [
   [6, 7, 8]
 ];
 
-const playerX = player()
-console.log(playerX.type('x'));
-console.log(playerX.type('o'))
-console.log(playerX.comMove(5));
+function gameBoard() {
+  const board = [];
+  let count = 0;
+
+  function placeMark(index, mark) {
+    board[index] = mark;
+  }
+
+  function type(n) {
+    return n;
+  }
+
+  function increaseMyCount() { 
+    return count++; 
+  };
+
+  function increaseBotcount() {
+    return count++;
+  }
+
+  return {
+    board,
+    placeMark,
+    type,
+    increaseMyCount,
+    increaseBotcount
+  };
+}
+
+const player = gameBoard();
+console.log(player.type('x'));
+console.log(player.type('o'))
 
 for (let i = 0; i < 9; i++) {
   const cell = document.createElement("button");
@@ -29,44 +59,59 @@ for (let i = 0; i < 9; i++) {
   board.appendChild(cell);
 }
 
+
 const items = document.querySelectorAll(".item");
 
-items.forEach((item) => {
-  item.addEventListener("click", (e) => {
-  if (xTurn) {
-    item.innerText = 'X';
-    xTurn = false;
-    msg.textContent = 'O Turn';
-  } else {
-    item.innerText = 'O';
-    xTurn = true;
-    msg.textContent = 'X Turn';
-  }
-  item.disabled = true;
-  restartBtn.addEventListner("click", (e) => {
+function playGame() {
+ items.forEach((item) => {
+   item.addEventListener("click", (e) => {
+     if (xTurn) {
+       item.innerText = 'X';
+       player.placeMark(e.target.dataset.index, 'X');
+       xTurn = false;
+       msg.textContent = 'O Turn';
+      } else {
+       item.innerText = 'O';
+       player.placeMark(e.target.dataset.index, 'O');
+       xTurn = true;
+       msg.textContent = 'X Turn';
+      }
+       item.disabled = true;
+       console.log("You clicked :", e.target.dataset.index);
+     })
+ });
+}
 
-  })
+playGame();
 
-   console.log("You clicked :", e.target.dataset.index);
- })
-})
-
+//to refresh the page
 newBtn.addEventListener("click", () => {
   location.reload();
 })
 
-
-
-
-function player() {
-  function type(n) {
-    return n;
-  }
-  function comMove(ran) {
-    return Math.floor(Math.random() * ran);
-  }
-  return {
-    type, 
-    comMove
-  };
+//choose between X and O
+function playerChoice() {
+  choiceBtn.forEach(button => {
+    button.addEventListener("click", (e) => {
+     const choice = e.currentTarget.value;
+     console.log(player.type(choice));
+    })
+  });
 }
+playerChoice();
+
+//check for a winner
+function checkWinner() {
+  player.board 
+  winPatterns
+  const type = [X, O];
+  for (const pattern of player.board) {
+
+  }
+
+}
+
+
+
+
+
