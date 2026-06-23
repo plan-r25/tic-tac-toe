8b8794538a9e444c18ee05b7d024bc22457764a5
planR <rplann24@gmail.com>
2026-06-23 16:06:12 +0000

    finally understand all the game logic

--- script.js
+++ script.js
@@ -1,188 +1,143 @@
-// const myScore = document.querySelector(".myScore");
-// const bScore = document.querySelector(".bScore");
-// const msg = document.querySelector("#msg");
+const myScore = document.querySelector(".myScore");
+const botScore = document.querySelector(".bScore");
+const msg = document.querySelector("#msg");
  const board = document.querySelector("#board");
-// const restartBtn = document.querySelector(".restart");
-// const newBtn = document.querySelector(".new-game");
+const restartBtn = document.querySelector(".restart");
+const newBtn = document.querySelector(".new-game");
 // const choiceBtn = document.querySelectorAll(".btn");
 
+const originalText = msg.textContent;
 let turn = true;
+let gameover = true;
+
+function scoreBoard() {
+  let score = 0;
+
+  const xScore = () => {score++};
+  const oScore = () => {score++};
+  const getScore = () => score;
+
+  return {
+    xScore,
+    oScore,
+    getScore
+  };
+}
+
+const xscore = scoreBoard();
+const oscore = scoreBoard();
 
 const Gameboard = {
-  gameboard: [
-    [" ", " ", " "],
-    [" ", " ", " "],
-    [" ", " ", " "]
-  ],
+  gameboard: ['', '', '', '', '', '', '', '', ''],
+  placemark(ind, mark) {
+    this.gameboard[ind] = mark;
+  },
   winPatterns: [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
     [0, 3, 6],
+    [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6],
-  ]
+  ],
+  isGameOver() {
+    for(let pattern of this.winPatterns) {
+      let val1 = this.gameboard[pattern[0]];
+      let val2 = this.gameboard[pattern[1]];
+      let val3 = this.gameboard[pattern[2]];
+      if (val1 !== "" && val1 === val2 && val2 === val3) {
+        msg.textContent = `${val1} wins`;
+        items.forEach(btn => btn.disabled = true);
+        setTimeout(() => {
+          items.forEach(btn => {
+            btn.innerText = "";
+            btn.disabled = false;
+        });
+          this.gameboard = ['', '', '', '', '', '', '', '', ''];
+          msg.textContent = originalText;
+          turn = true;
+        }, 2000)
+        return true;
+      }
+    }
+  },
+  computer() {
+    const emptyBtn = Array.from(items).filter(item => item.innerText === "");
+
+    if(emptyBtn.length === 0) return;
+
+    const ran = Math.floor(Math.random() * emptyBtn.length);
+    const chooseBtn = emptyBtn[ran];
+
+    const actIndex = Number(chooseBtn.dataset.index);
+
+    chooseBtn.innerText = "O";
+    this.placemark(actIndex, "O");
+    chooseBtn.disabled = true;
+
+    if(this.isGameOver()) {
+      oscore.oScore();
+      botScore.textContent = oscore.getScore();
+      return;
+    };
+    msg.textContent = "X turn";
+
+    turn = true;
+  },
+  resartGame: (function() {
+    restartBtn.addEventListener("click", () => {
+      items.forEach(btn => {
+        btn.innerText = "";
+        btn.disabled = false;
+      });
+      this.gameboard = ['', '', '', '', '', '', '', '', ''];
+      msg.textContent = originalText;
+      turn = true;
+    })
+  })(),
+  newGame: (function() {
+    newBtn.addEventListener("click", () => {
+      location.reload();
+    })
+  })()
 };
 
-//for loop to create html btns
+//for loop to create html buttons
 for (let i = 0; i < 9; i++) {
   const btn = document.createElement("button");
   btn.classList.add("item");
   btn.dataset.index = i;
-  console.log(btn.dataset.index);
+  // console.log(btn.dataset.index);
   // btn.textContent = i;
   board.appendChild(btn);
 };
 
 const items = document.querySelectorAll(".item");
 
-(function game() {
+//function to push index btn into the gameboard arr
+(function move() {
   items.forEach((btn) => {
     btn.addEventListener("click", (e) => {
-    if(turn) {
-      if(btn.dataset.index === '0') {
-        btn.innerText = "x";
-        Gameboard.gameboard[0][0] = btn.textContent;
+      if (turn && btn.innerText === "") {
+        btn.innerText = "X"
+        
+        let ind = Number(e.currentTarget.dataset.index);
+        Gameboard.placemark(ind, "X");
+        btn.disabled = true;
+        turn = false;
+        if (Gameboard.isGameOver()) {
+          xscore.xScore();
+          myScore.textContent = xscore.getScore();
+          return;
+        };
+        msg.textContent = "O trun";
+        setTimeout(() => {
+         Gameboard.computer();
+        }, 1000)
       }
-    }
-    })
+    });
   })
-})()
+})();
 
-//node js
-// const board = [[" ", " ", " "], 
-//                [" ", " ", " "],
-//                [" ", " ", " "],
-// ]
-
-// let m = [];
-// let turn = true;
-// let combo = {
-//   row: undefined,
-//   col: undefined
-// }
-
-// const allequal = arr => arr.every(v => v !== " " && v === arr[0]);
-
-// const readline = require("readline")
-
-// let gameOver = false;
-// let winner = '';
-
-// const game = {
-//   update: function(){
-//     this.isgameOver();
-
-//     if(gameOver) {
-//       this.updateBoard()
-//       console.log(`Game over ${winner} won!`);
-//       process.exit();
-//     }
-//     this.updateBoard();
-//     m = this.possibleMoves();
-
-//     if(m.length === 0) {
-//       gameOver = true;
-//       console.log("Game over by draw");
-//       process.exit();
-//     }
-//   },
-
-//   isgameOver: function(){
-
-//     if(allequal(board[0])) {
-//       gameOver = true;
-//       winner = board[0][0]
-//     }
-//      if(allequal(board[1])) {
-//       gameOver = true;
-//       winner = board[1][0]
-//      }
-//       if(allequal(board[2])) {
-//         gameOver = true;
-//         winner = board[2][0]
-//       }
-
-//       if(allequal([board[0][0], board[1][0], board[2][0]])) {
-//         gameOver = true;
-//         winner = board[0][0]
-//       }
-//        if(allequal([board[0][1], board[1][1], board[2][1]])) {
-//         gameOver = true;
-//         winner = board[0][1]
-//        }
-//         if(allequal([board[0][2], board[1][2], board[2][2]])) {
-//           gameOver = true;
-//           winner = board[0][2]
-//         }
-//          if(allequal([board[0][0], board[1][1], board[2][2]])) {
-//            gameOver = true;
-//            winner = board[0][0]
-//          }
-//           if(allequal([board[0][2], board[1][1], board[2][0]])) {
-//             gameOver = true;
-//             winner = board[0][2]
-//           }
-
-//   },
-//   move: function(c){
-//     board[+c.row][+c.col] = "X"
-
-//     combo.row = undefined
-//     combo.col = undefined
-//     this.update()
-//     this.computer()
-//   },
-//   possibleMoves: function(){
-//     const p = []
-
-//     for(let i = 0; i < board.length; i++) {
-//       for(let j = 0; j < board[0].length; j++){
-//         if(board[i][j] === " "){
-//           p.push({row: i, col: j})
-//         }
-//       }
-//     }
-//      return p;
-//   },
-//   computer: function(){
-
-//     console.log(m) 
-//     if(m.length > 0){
-//       let ra = Math.round(Math.random() * (m.length - 1))
-//       board[m[ra].row][m[ra].col] = "O";
-//     }
-//     turn = true;
-//     this.update();
-//     console.log("your turn");
-//   },
-
-//   updateBoard: function(){
-//     console.log("Updating board")
-//     board.forEach((arr, i)=> {
-//       console.log(arr.toString().replace(/,/g, "|"))
-//     })
-//   }
-// }
- 
-// readline.emitKeypressEvents(process.stdin);
-// process.stdin.setRawMode(true);
-
-// process.stdin.on('keypress', (str, key) => {
-//   if (key.ctrl && key.name === 'c') {
-//     process.exit();
-//   } else {
-//     if(turn) {
-//       if(combo.row){
-//         combo.col = key.name
-//         turn = false
-//         game.move(combo)
-//       } else {
-//         combo.row = key.name
-//       }
-//     } else {
-//       console.log("wait your turn");
-//     }
-//   }
-// });
